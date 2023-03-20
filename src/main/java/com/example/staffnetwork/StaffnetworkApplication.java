e586515b185d788038d07f6f6ff6dbbe3edce8c5
TasrulMostafiz <tasrulmostafiz91@gmail.com>
2023-03-20 12:23:47 +0600

    Initial Commit

---
+++ src/main/java/com/example/staffnetwork/StaffnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.staffnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/staffnetwork/StaffnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staffnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
