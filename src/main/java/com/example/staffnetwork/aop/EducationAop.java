8e0d9b6c09896e82d375eef5166c4ab9a4514cce
TasrulMostafiz <tasrulmostafiz91@gmail.com>
2023-03-22 12:16:28 +0600

    EducationAOP Added

---
+++ src/main/java/com/example/staffnetwork/aop/EducationAop.java
@@ -0,0 +1,42 @@
+package com.example.staffnetwork.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component 
+public class EducationAop {
+    Logger logger = LoggerFactory.getLogger(EducationAop.class);
+
+    @Before("execution(* com.example.staffnetwork.service.impl.EducationServiceImpl+.*(..))")
+    public void before(JoinPoint joinPoint){
+        logger.info("Before - "+joinPoint.getSignature().getName());
+        logger.info("Before - "+joinPoint.getTarget().getClass());
+        logger.info("Before - "+ Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("execution(* com.example.staffnetwork.service.impl.EducationServiceImpl+.*(..))")
+    public void after(JoinPoint joinPoint){
+        logger.info("After - "+joinPoint.getTarget().getClass());
+        logger.info("After - "+joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(value = "execution(* com.example.staffnetwork.service.impl.EducationServiceImpl+.*(..))",returning = "result")
+    public void afterReturning(JoinPoint joinPoint,Object result){
+        logger.info("After Return - "+joinPoint.getTarget().getClass());
+        logger.info("After Return - "+joinPoint.getSignature().getName());
+        logger.info("Returned - "+result);
+    }
+
+    @AfterThrowing(value = "execution(* com.example.staffnetwork.service.impl.EducationServiceImpl+.*(..))",throwing = "ex")
+    public void afterThrowing(JoinPoint joinPoint,Exception ex){
+        logger.info("After Throwing - "+joinPoint.getTarget().getClass());
+        logger.info("After Throwing - "+joinPoint.getSignature().getName());
+        logger.info("Exception - "+ex.getMessage());
+    }
+}
